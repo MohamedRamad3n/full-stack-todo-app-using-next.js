0e47d28ef44d12b90bfc27632e6741aaf37ea125
Mohamed Ramadan <93547459+MohamedRamad3n@users.noreply.github.com>
2025-04-24 18:12:51 +0200

    Setup Clerk Authentication

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,17 +3,11 @@ import { faker } from '@faker-js/faker'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.user.createMany({
-        data: Array.from({ length: 10 }, () => ({
-            name: faker.internet.username(),
-            email: faker.internet.email(),
-            password: faker.internet.password(),
-        }))
-    })
     await prisma.todo.createMany({
         data: Array.from({ length: 10 }, () => ({
             title: faker.lorem.sentence(),
             description: faker.lorem.paragraph(),
+            userId: faker.string.uuid(),
         }))
     })
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/provider/theme-Provider";
 import { Toaster } from "sonner";
-
+import { ClerkProvider } from "@clerk/nextjs";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,21 +31,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          suppressHydrationWarning
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        > 
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
